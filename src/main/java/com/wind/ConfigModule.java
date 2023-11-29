b35d812e7e013038ee21e346598a81808f352d2d
PhongTn <phongtn.group1@gmail.com>
2023-11-29 22:45:04 +0700

    add DI module

---
+++ src/main/java/com/wind/ConfigModule.java
@@ -0,0 +1,28 @@
+package com.wind;
+
+import com.google.api.client.http.HttpTransport;
+import com.google.api.client.http.javanet.NetHttpTransport;
+import com.google.api.client.json.JsonFactory;
+import com.google.api.client.json.gson.GsonFactory;
+import com.google.inject.AbstractModule;
+import com.google.inject.name.Names;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.io.IOException;
+import java.util.Properties;
+
+public class ConfigModule extends AbstractModule {
+
+    @Override
+    protected void configure() {
+        try {
+            Properties property = new Properties();
+            ClassLoader classLoader = Thread.currentThread().getContextClassLoader();
+            property.load(classLoader.getResourceAsStream("application.properties"));
+            Names.bindProperties(binder(), property);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+}

df9cf059ea7caf9de8a21c89375df06b72634d9f
PhongTn <phongtn.group1@gmail.com>
2023-12-26 15:30:02 +0700

    method utilities to convert bytes to Human-readable

--- src/main/java/util/StreamUtil.java
+++ src/main/java/util/StreamUtil.java
@@ -9,15 +9,17 @@ import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.net.URL;
 import java.nio.file.Files;
+import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.nio.file.StandardCopyOption;
+import java.text.CharacterIterator;
+import java.text.StringCharacterIterator;
 
 public class StreamUtil {
 
     private final Logger logger = LoggerFactory.getLogger(this.getClass().getName());
 
-    public InputStream getFileAsIOStream(final String fileName)
-    {
+    public InputStream getFileAsIOStream(final String fileName) {
         InputStream ioStream = this.getClass()
                 .getClassLoader()
                 .getResourceAsStream(fileName);
@@ -28,11 +30,9 @@ public class StreamUtil {
         return ioStream;
     }
 
-    public void printFileContent(InputStream is) throws IOException
-    {
+    public void printFileContent(InputStream is) throws IOException {
         try (InputStreamReader isr = new InputStreamReader(is);
-             BufferedReader br = new BufferedReader(isr);)
-        {
+             BufferedReader br = new BufferedReader(isr);) {
             String line;
             while ((line = br.readLine()) != null) {
                 logger.info(line);
@@ -43,11 +43,28 @@ public class StreamUtil {
 
     /**
      * <a href="https://www.baeldung.com/java-download-file">...</a>
+     *
      * @param fileName
      * @param fileUrl
      */
     public void downloadFile(String fileName, String fileUrl) throws IOException {
         InputStream in = new URL(fileUrl).openStream();
-        Files.copy(in, Paths.get(fileName), StandardCopyOption.REPLACE_EXISTING);
+        Path target = Paths.get(fileName);
+        Files.copy(in, target, StandardCopyOption.REPLACE_EXISTING);
+    }
+
+    public static String humanReadableByteCountBin(long bytes) {
+        long absB = bytes == Long.MIN_VALUE ? Long.MAX_VALUE : Math.abs(bytes);
+        if (absB < 1024) {
+            return bytes + " B";
+        }
+        long value = absB;
+        CharacterIterator ci = new StringCharacterIterator("KMGTPE");
+        for (int i = 40; i >= 0 && absB > 0xfffccccccccccccL >> i; i -= 10) {
+            value >>= 10;
+            ci.next();
+        }
+        value *= Long.signum(bytes);
+        return String.format("%.1f %ciB", value / 1024.0, ci.current());
     }
 }
